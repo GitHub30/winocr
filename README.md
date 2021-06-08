df89d908f9c41e249208dbe51d774caae7221ee9
Tomofumi Inoue <funaox@gmail.com>
2021-06-08 20:19:48 +0900

    first

---
+++ README.md
@@ -0,0 +1,55 @@
+# WinOCR
+
+# Installation
+```powershell
+pip install winocr
+```
+
+# Usage
+## Pillow
+```python
+import winocr
+from PIL import Image
+
+img = Image.open('test.jpg')
+print((await winocr.recognize_pil(img, 'ja')).text)
+```
+
+## OpenCV
+```python
+import winocr
+import cv2
+
+img = cv2.imread('test.jpg')
+print((await winocr.recognize_cv2(img, 'ja')).text)
+```
+
+## Web API
+Run server
+```powershell
+winocr_serve
+```
+
+### curl
+```bash
+curl localhost:8000?lang=ja --data-binary @test.jpg
+```
+
+### Python
+```python
+import requests
+
+bytes = open('test.jpg', 'rb').read()
+requests.post('http://localhost:8000/?lang=ja', bytes).json()['text']
+```
+
+### JavaScript
+```javascript
+// File
+const file = document.querySelector('[type=file]').files[0]
+await fetch('http://localhost:8000/', {method: 'POST', body: file}).then(r => r.json())
+
+// Blob
+const blob = await fetch('https://image.itmedia.co.jp/ait/articles/1706/15/news015_16.jpg').then(r=>r.blob())
+await fetch('http://localhost:8000/?lang=ja', {method: 'POST', body: blob}).then(r => r.json())
+```
---
+++ winocr.py
@@ -0,0 +1,45 @@
+from winrt.windows.media.ocr import OcrEngine
+from winrt.windows.globalization import Language
+from winrt.windows.storage.streams import DataWriter
+from winrt.windows.graphics.imaging import SoftwareBitmap, BitmapPixelFormat
+
+def recognize_bytes(bytes, width, height, lang='en'):
+    cmd = 'Add-WindowsCapability -Online -Name "Language.OCR~~~en-US~0.0.1.0"'
+    assert OcrEngine.is_language_supported(Language(lang)), cmd
+    writer = DataWriter()
+    writer.write_bytes(list(bytes))
+    sb = SoftwareBitmap.create_copy_from_buffer(writer.detach_buffer(), BitmapPixelFormat.RGBA8, width, height)
+    return OcrEngine.try_create_from_language(Language(lang)).recognize_async(sb)
+
+def recognize_pil(img, lang='en'):
+    if img.mode != 'RGBA':
+        img = img.convert('RGBA')
+    return recognize_bytes(img.tobytes(), img.width, img.height, lang)
+
+def recognize_cv2(img, lang='en'):
+    import cv2
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    return recognize_bytes(img.tobytes(), img.shape[1], img.shape[0], lang)
+
+def _serializer(o):
+    from winrt.windows.foundation.collections import IVectorView
+    return list(o) if isinstance(o, IVectorView) else dict([(n, getattr(o, n)) for n in dir(o) if not n.startswith('_')])
+
+def serve():
+    import json
+    import uvicorn
+    from PIL import Image
+    from io import BytesIO
+    from fastapi import FastAPI, Request, Response
+    from fastapi.middleware.cors import CORSMiddleware
+
+    app = FastAPI()
+    app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
+    @app.post('/')
+    async def recognize(request: Request, lang: str = 'en'):
+        result = await recognize_pil(Image.open(BytesIO(await request.body())), lang)
+        return Response(json.dumps(result, default=_serializer, indent=2, ensure_ascii=False), media_type='application/json')
+    uvicorn.run(app, host='0.0.0.0')
+
+if __name__ == '__main__':
+    serve()
