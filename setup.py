b6992565cc725e284e89f70f758c3ea6b3082739
Tomofumi Inoue <funaox@gmail.com>
2021-06-08 22:26:38 +0900

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ all = api + cv2
 
 setuptools.setup(
     name='winocr',
-    version='0.0.6',
+    version='0.0.7',
     author='Tomofumi Inoue',
     author_email='funaox@gmail.com',
     description='Windows.Media.Ocr',
